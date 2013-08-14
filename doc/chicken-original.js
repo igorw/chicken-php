dafb195f64d23da66ac7c53861c91340c06b7142
Igor Wiedler <igor@wiedler.ch>
2013-08-14 16:10:44 +0200

    Add original chicken.js source

---
+++ doc/chicken-original.js
@@ -0,0 +1,54 @@
+// chicken.js
+// http://torso.me/chicken
+// (c) 2013 Torbjörn Söderstedt
+
+function chicken(CHICKEN, Chicken) {
+    Chicken &&( chicken. chicken =[,
+    CHICKEN, CHICKEN = Chicken = chicken.
+    $Chicken =-( CHICKEN ==( chicken.
+    Chicken = Chicken ))], chicken.
+    chicken [Chicken++] = chicken. chicken, chicken.
+    CHICKEN = ++Chicken, chicken (--Chicken), chicken.
+    $Chicken = ++Chicken, chicken. CHICKEN++ );
+    Chicken = chicken. Chicken [chicken.
+    $Chicken++ ]; chicken. Chicken = CHICKEN? Chicken?
+    '\012'== Chicken? chicken (++ CHICKEN, chicken.
+    chicken [++ chicken. CHICKEN ]=
+    CHICKEN - CHICKEN ): Chicken
+    ==' '|'\015'== Chicken ||
+    (Chicken   )== "c" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "h" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "i" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "c" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "k" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "e" &  chicken. Chicken [chicken.
+    $Chicken++ ]== "n"&&++chicken. chicken [chicken.
+    CHICKEN]? chicken (CHICKEN)
+    :[ "Error on line "+CHICKEN+": expected 'chicken'",
+       chicken. CHICKEN = CHICKEN ++- CHICKEN ]:
+    chicken. chicken :( CHICKEN = chicken.
+    Chicken[chicken.CHICKEN], Chicken? (Chicken =
+
+    --Chicken? --Chicken? --Chicken? --Chicken? --Chicken?
+    --Chicken? --Chicken? --Chicken? --Chicken?
+    chicken. CHICKEN++ &&
+    --Chicken :'&#'+CHICKEN+';': chicken.
+    Chicken [chicken. Chicken [-- chicken. CHICKEN ]&&
+    (chicken. $Chicken += CHICKEN), --chicken.
+    CHICKEN ]: chicken. Chicken [chicken.
+    Chicken [CHICKEN] = chicken. Chicken
+    [-- chicken. CHICKEN ],-- chicken. CHICKEN ]:
+    chicken. Chicken [chicken. Chicken [chicken.
+    $Chicken++ ]] [CHICKEN]: CHICKEN == chicken.
+    Chicken [-- chicken. CHICKEN ]:
+    CHICKEN*chicken. Chicken [-- chicken.
+    CHICKEN ]: chicken. Chicken [-- chicken.
+    CHICKEN ]- CHICKEN: chicken. Chicken [-- chicken.
+    CHICKEN ]+ CHICKEN: chicken.
+    CHICKEN ++ && "chicken", chicken.
+    Chicken [chicken. CHICKEN ]= Chicken, chicken
+    ()): CHICKEN );
+
+    return chicken.
+    Chicken
+}
