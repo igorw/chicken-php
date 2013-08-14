ba8361931ee696d2b2109f8687e01a038ad7e4c6
Igor Wiedler <igor@wiedler.ch>
2013-08-14 18:55:14 +0200

    CS

--- doc/calculator.c
+++ doc/calculator.c
@@ -57,7 +57,7 @@ double val[MAXVAL]; /* value stack */
 /* push: push f onto value stack */
 void push(double f)
 {
-    if ( sp < MAXVAL)
+    if (sp < MAXVAL)
         val[sp++] = f;
     else
         printf("error: stack full. can't push %g\n", f);
