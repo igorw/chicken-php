867fea330514a815bdaeb1e460e48130e5330e7f
Igor Wiedler <igor@wiedler.ch>
2013-08-17 01:51:03 +0200

    More detailed/granular debug statements

--- src/vm.php
+++ src/vm.php
@@ -26,26 +26,6 @@ const OP_STORE = 7;
 const OP_JUMP = 8;
 const OP_CHAR = 9;
 
-function format_opcode($opcode) {
-    $mapping = [
-        OP_EXIT     => 'exit',
-        OP_CHICKEN  => 'chicken',
-        OP_ADD      => 'add',
-        OP_SUBTRACT => 'subtract',
-        OP_MULTIPLY => 'multiply',
-        OP_COMPARE  => 'compare',
-        OP_LOAD     => 'load',
-        OP_STORE    => 'store',
-        OP_JUMP     => 'jump',
-        OP_CHAR     => 'char',
-    ];
-
-    if (isset($mapping[$opcode]))
-        return $mapping[$opcode];
-
-    return sprintf('push %s', $opcode - 10);
-}
-
 function format_stack(array $stack, $stack_start, $stack_end) {
     return array_slice($stack, $stack_start, $stack_end - $stack_start + 1);
 }
@@ -77,9 +57,10 @@ class Machine {
             $opcode = $this->next_opcode();
             $this->process_opcode($opcode);
 
-            $this->logger->debug(sprintf('exec %s', format_opcode($opcode)));
             $this->logger->debug('stack', ['stack' => format_stack($this->stack, $this->stack_start, $this->sp)]);
         }
+
+        $this->logger->debug('exec exit');
 
         return $this->peek();
     }
@@ -96,43 +77,55 @@ class Machine {
         switch ($opcode) {
             case OP_CHICKEN:
                 $this->push('chicken');
+                $this->logger->debug('exec chicken');
                 break;
             case OP_ADD:
                 $head = $this->pop();
                 $value = $this->plus($this->pop(), $head);
                 $this->push($value);
+                $this->logger->debug('exec add');
                 break;
             case OP_SUBTRACT:
                 $head = $this->pop();
                 $this->push($this->pop() - $head);
+                $this->logger->debug('exec subtract');
                 break;
             case OP_MULTIPLY:
                 $this->push($this->pop() * $this->pop());
+                $this->logger->debug('exec multiply');
                 break;
             case OP_COMPARE:
                 $this->push($this->pop() == $this->pop());
+                $this->logger->debug('exec compare');
                 break;
             case OP_LOAD:
                 $sourcep = $this->next_opcode();
                 $source = $this->stack[$sourcep];
                 $head = $this->pop();
                 $this->push(isset($source[$head]) ? $source[$head] : null);
+                $this->logger->debug(sprintf('exec load %s', $sourcep));
                 break;
             case OP_STORE:
                 $head = $this->pop();
                 $this->stack[$head] = $this->pop();
+                $this->logger->debug('exec store');
                 break;
             case OP_JUMP:
                 $head = $this->pop();
                 if ($this->pop()) {
                     $this->ip += $head;
+                    $this->logger->debug(sprintf('exec jump %s', $head > 0 ? "+$head" : $head));
+                } else {
+                    $this->logger->debug('exec jump skip');
                 }
                 break;
             case OP_CHAR:
                 $this->push(chr($this->pop()));
+                $this->logger->debug('exec char');
                 break;
             default:
                 $this->push($opcode - 10);
+                $this->logger->debug(sprintf('exec push %s', $opcode - 10));
                 break;
         }
     }
